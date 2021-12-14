7bffa1bf11c93a7b3429f93c9cadc01b595b5b25
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2021-12-14 14:45:31 +0200

    housekeeping

--- day-5.rkt
+++ day-5.rkt
@@ -4,8 +4,7 @@
   (with-input-from-file "day-5-1.txt"
     (lambda ()
       (define (produce-line x1 y1 x2 y2)
-        (if (and (equal? x1 x2)
-                 (equal? y1 y2))
+        (if (and (equal? x1 x2) (equal? y1 y2))
             (list (cons x1 y1))
             (cons (cons x1 y1)
                   (produce-line (+ (sgn (- x2 x1)) x1) (+ (sgn (- y2 y1)) y1)
@@ -18,12 +17,8 @@
                              (map string->number (string-split start-end ",")))
                            (string-split l " -> "))))))))
 
-(define (compute-coverings lines)
-  (define covered (make-hash))
-  
-  (for ([point (apply append lines)])
-    (hash-set! covered point (add1 (hash-ref! covered point 0))))
-  covered)
+(define (covered-points lines)
+  (group-by identity (apply append lines)))
 
 (define (vertical? line)
   (equal? (car (first line)) (car (second line))))
@@ -32,12 +27,12 @@
   (equal? (cdr (first line)) (cdr (second line))))
 
 (displayln (format "number of points where 2 or more (horizontal or vertical) vent lines cross:\n~a"
- (count (lambda (x) (< 1 x))
-        (hash-values (compute-coverings (filter (lambda (line)
-                                                  (or (vertical? line)
-                                                      (horizontal? line)))
-                                                vent-lines))))))
+ (count (lambda (covered) (< 1 (length covered)))
+        (covered-points (filter (lambda (line)
+                                  (or (vertical? line) (horizontal? line)))
+                                vent-lines)))))
+(newline)
 
 (displayln (format "number of points where 2 or more vent lines cross:\n~a"
- (count (lambda (x) (< 1 x))
-        (hash-values (compute-coverings vent-lines)))))
+ (count (lambda (covered) (< 1 (length covered)))
+        (covered-points vent-lines))))
