2e2d99585a4450245b4ea391f5850d00bcba11a4
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2021-12-14 18:18:27 +0200

    housekeeping

--- day-12.rkt
+++ day-12.rkt
@@ -5,17 +5,12 @@
     (lambda ()
       (define cave->cave (make-hash))
       
-      (define (add-bipath a b)
-        (if (hash-has-key? cave->cave a)
-            (hash-set! cave->cave a (cons b (hash-ref cave->cave a)))
-            (hash-set! cave->cave a (cons b (list))))
-        (if (hash-has-key? cave->cave b)
-            (hash-set! cave->cave b (cons a (hash-ref cave->cave b)))
-            (hash-set! cave->cave b (cons a (list)))))
+      (define (connect-caves a b)
+        (hash-set! cave->cave a (cons b (hash-ref! cave->cave a (list))))
+        (hash-set! cave->cave b (cons a (hash-ref! cave->cave b (list)))))
       
       (for ([l (in-lines)])
-        (define split (string-split l "-"))
-        (add-bipath (first split) (second split)))
+        (apply connect-caves (string-split l "-")))
       cave->cave)))
 
 (define (downcase? str)
@@ -29,28 +24,22 @@
   (let loop ([paths paths])
     (define extended 0)
     (define extended-paths
-      (apply append
-             (for/list ([path paths])
-               (cond [(equal? "end" (car path))
-                      (list path)]
-                     [else
-                      (set! extended (add1 extended))
-                      
-                      (define next-caves
-                        (hash-ref adjacencies (car path)))
-                      (for/list ([next next-caves]
-                                 #:when (valid-transition? next path))
-                        (cond [(and (downcase? next)
-                                    (member next path))
-                               (cons next (cons 'twice path))]
-                              [(equal? "end" next)
-                               (cons next (remove 'twice path))]
-                              [else
-                               (cons next path)]))]))))
-    
+      (for/list ([path paths])
+        (cond [(equal? "end" (car path)) (list path)]
+              [else
+               (set! extended (add1 extended))
+               (for/list ([next-cave (hash-ref adjacencies (car path))]
+                          #:when (valid-transition? next-cave path))
+                 (cond [(and (downcase? next-cave)
+                             (member next-cave path))
+                        (cons next-cave (cons 'twice path))]
+                       [(equal? "end" next-cave)
+                        (cons next-cave (remove 'twice path))]
+                       [else
+                        (cons next-cave path)]))])))
     (if (zero? extended)
         paths
-        (loop extended-paths))))
+        (loop (apply append extended-paths)))))
 
 (define transition-fun1
   (lambda (next path)
@@ -58,10 +47,10 @@
 
 (define transition-fun2
   (lambda (next path)
-    (or (and (not (member next '("start" "end")))
+    (or (transition-fun1 next path)
+        (and (not (member next '("start" "end")))
              (member next path)
-             (not (member 'twice path)))
-        (not (and (member next path) (downcase? next))))))
+             (not (member 'twice path))))))
 
 (displayln (format "number of paths that visit each small cave at most once:\n~a"
  (length (find-paths-in adjacent-caves #:where transition-fun1))))
