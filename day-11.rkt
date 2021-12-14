079dccad3f025946875e11fe1a9474f638fcfeb7
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2021-12-14 14:24:39 +0200

    housekeeping

--- day-11.rkt
+++ day-11.rkt
@@ -4,10 +4,10 @@
   (with-input-from-file "day-11-1.txt"
     (lambda ()
       (for/list ([l (in-lines)])
-          (map string->number (filter non-empty-string? (string-split l "")))))))
+        (map string->number (filter non-empty-string? (string-split l "")))))))
 
-(define H (length octopus-energy-levels))
-(define W (length (first octopus-energy-levels)))
+(define row-count (length octopus-energy-levels))
+(define col-count (length (first octopus-energy-levels)))
 
 (define (get-level energy-levels i j)
   (list-ref (list-ref energy-levels i) j))
@@ -15,15 +15,13 @@
 (define (neighbourhood i j)
   (for/list ([oi '(+1 +1 +1 +0 -1 -1 -1 +0)]
              [oj '(-1 +0 +1 +1 +1 +0 -1 -1)]
-             #:when (and (<= 0 (+ i oi) (sub1 H))
-                         (<= 0 (+ j oj) (sub1 W))))
+             #:when (and (<= 0 (+ i oi) (sub1 row-count))
+                         (<= 0 (+ j oj) (sub1 col-count))))
     (cons (+ i oi) (+ j oj))))
 
 (define (adjacent-flashes energy-levels i j)
   (for/sum ([neighbour (neighbourhood i j)])
-    (define ni (car neighbour))
-    (define nj (cdr neighbour))
-    (if (< 9 (get-level energy-levels ni nj)) 1 0)))
+    (if (< 9 (get-level energy-levels (car neighbour) (cdr neighbour))) 1 0)))
 
 (define (simulate-step energy-levels)
   (define part-1
@@ -35,18 +33,17 @@
                [total-flashes 0])
       (define flashcount 0)
       (define do-flashes
-        (for/list ([i H])
-          (for/list ([j W])
-            (define level
-              (get-level energy-levels i j))
+        (for/list ([i row-count])
+          (for/list ([j col-count])
+            (define level (get-level energy-levels i j))
             (cond [(< 9 level)
                    (set! flashcount (add1 flashcount))
                    -inf.0]
                   [else
                    (+ level (adjacent-flashes energy-levels i j))]))))
-        (if (zero? flashcount)
-            (cons do-flashes total-flashes)
-            (loop do-flashes (+ total-flashes flashcount)))))
+      (if (zero? flashcount)
+          (cons do-flashes total-flashes)
+          (loop do-flashes (+ total-flashes flashcount)))))
 
   (define part-2->3
     (for/list ([row (car part-1->2)])
@@ -59,9 +56,7 @@
   (cond [(zero? steps)
          (cons energy-levels flashes )]
         [else
-         (define one-step
-           (simulate-step energy-levels))
-
+         (define one-step (simulate-step energy-levels))
          (simulate (sub1 steps) (car one-step) (+ flashes (cdr one-step)))]))
 
 (define (find-synchronisation energy-levels [steps 0])
@@ -70,13 +65,12 @@
                  energy-levels)
          (cons energy-levels steps)]
         [else
-         (define one-step
-           (simulate-step energy-levels))
+         (define one-step (simulate-step energy-levels))
          (find-synchronisation (car one-step) (add1 steps))]))
 
 (displayln (format "number of flashes after 100 steps:\n~a"
  (cdr (simulate 100 octopus-energy-levels))))
+(newline)
 
 (displayln (format "number of steps until octopuses synchronise:\n~a"
  (cdr (find-synchronisation octopus-energy-levels))))
-
