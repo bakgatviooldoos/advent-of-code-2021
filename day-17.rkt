2e73fbb9e5947b19aae32b9db6517c4f533f2f4c
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2021-12-17 20:21:00 +0200

    housekeeping

--- day-17.rkt
+++ day-17.rkt
@@ -16,11 +16,9 @@
       (values (parse-range (first split-x-y))
               (parse-range (second split-x-y))))))
 
-(define (crosses-target? vx vy)
-  (let loop ([px 0]
-             [py 0]
-             [vx vx]
-             [vy vy])
+(define (crosses-target? initial-vx initial-vy)
+  (let loop ([px 0]          [py 0]
+             [vx initial-vx] [vy initial-vy])
     (cond [(or (< (cdr x-range) px)
                (< py (car y-range)))
            #f]
@@ -28,16 +26,17 @@
                 (<= (car y-range) py (cdr y-range)))
            #t]
           [else
-           (loop (+ px vx) (+ py vy) (- vx (sgn vx)) (- vy 1))])))
+           (loop (+ px vx)       (+ py vy)
+                 (- vx (sgn vx)) (- vy 1))])))
 
 (define valid-initial-velocities
- (for*/list ([vx (in-range (add1 (cdr x-range)))]
+ (for*/list ([vx (in-range 0             (add1 (cdr x-range)))]
              [vy (in-range (car y-range) (- (car y-range)))]
              #:when (crosses-target? vx vy))
    (cons vx vy)))
 
-(define (apex vy)
-  (/ (* vy (+ vy 1)) 2))
+(define (apex initial-vy)
+  (/ (* initial-vy (+ initial-vy 1)) 2))
 
 (displayln (format "highest y-position reached in any valid trajectory:\n~a"
  (apex (cdr (argmax cdr valid-initial-velocities)))))
